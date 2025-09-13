252f71bab916a75f7f848674829cc1337a41a63b
fevunge <fevunge@outlook.com>
2025-09-14 00:22:08 +0100

    learn: create a form with react commponents and tailwind css

--- module-01/app.js
+++ module-01/app.js
@@ -3,7 +3,7 @@ const buttonVariants = {
   black: 'bg-black',
 };
 
-fucntion Input({label, type, ...props}) {
+function Input({label, type, ...props}) {
   return (
     <div className="m-4">
       <label className="block text-gray-700 text-sm font-bold mb-2">
@@ -16,7 +16,7 @@ fucntion Input({label, type, ...props}) {
 
 function Button({children, variant='blue', ...props}) {
   return (
-    <button className={`${buttonVariants[variant]} text-white font-bold py-1 px-4 m-4 rounded`} {...props}>
+    <button className={`${buttonVariants[variant]} text-white font-bold py-2 px-4 m-1 rounded w-full`} {...props}>
       {children}
     </button>
   );
@@ -24,10 +24,10 @@ function Button({children, variant='blue', ...props}) {
 
 function Form() {
   return (
-    <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    <form className="bg-white px-8 pt-6 pb-8 mb-4 w-full">
       <Input label="Username" type="text" placeholder="Enter your username" />
       <Input label="Password" type="password" placeholder="Enter your password" />
-      <div className="flex items-center justify-between">
+      <div className="flex flex-col items-center align-center">
         <Button>Login</Button>
         <Button variant="black">Sign Up</Button>
       </div>
@@ -38,7 +38,7 @@ function Form() {
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-medium">
+      <h1 className="font-medium text-2xl text-center my-4 font-bold">
         Welcome to JSX World
       </h1>
       <Form />
