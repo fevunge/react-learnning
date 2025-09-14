4c00ad8e57185044781578a860adfc90c16c303e
fevunge <fevunge@outlook.com>
2025-09-14 01:51:46 +0100

    learn: create calculator commponent

--- module-02/app.js
+++ module-02/app.js
@@ -1,5 +1,4 @@
 
-
 const textVariant = {
   default: "text-xl",
   muted: "text-xl text-(--text-secondary)",
@@ -7,15 +6,25 @@ const textVariant = {
   blast: "text-3xl font-medium",
 }
 
-function Text({as = 'span', children, className, variant, ...props}) {
+const buttonVariant = {
+  default: "bg-(--background)",
+  primary: "bg-(--primary)",
+}
+
+function Text({as = 'span', children, className, variant='default', ...props}) {
   const Component = as;
   return <Component className={`${textVariant[variant]}  ${className}`} {...props}>{children}</Component>;
 }
 
-function Button({children, className, ...props}) {
+function Button({children, variant='default', className, ...props}) {
   return (
-    <Text as='button' variant='heading' 
-      className={`px-4 py-2 flex items-center justify-center rounded-xl cursor-pointer text(--text) bg-linear-(--gradient) hover:bg-linear-(--gradient-hover) ${className}`} 
+    <Text 
+      as='button' 
+      variant='heading'
+      className={`${buttonVariant[variant]} px-4 py-2 flex items-center justify-center rounded-xl cursor-pointer 
+        text-(--text) bg-linear-(--gradient) hover:bg-linear-(--gradient-hover) shadow-(--shadow) 
+        ${className}`
+      } 
       {...props}
     >
       {children}
@@ -23,11 +32,42 @@ function Button({children, className, ...props}) {
   );
 }
 
+function Card({children, className, ...props}) {
+  return (
+    <div 
+      className={
+        `bg-(--background) py-16 px-4 rounded-2xl shadow-(--shadow)  ${className}`
+      } 
+      {...props}
+    >
+      {children}
+    </div>
+  );
+}
+
+
+
+function Calculator(params) {
+  return (
+    <Card className="flex flex-col gap-[1.625rem] w-[22.75rem] py-12 px-8">
+      <div className="flex flex-col gap-2 px-[1.375rem] cursor-default select-none">
+        <Text variant="muted" className="flex items-center justify-end">
+          2 + 3
+        </Text>
+        <div className="flex items-center justify-between">
+          <Text variant="muted" className="flex items-center">=</Text>
+          <Text variant="blast" className="flex items-center justify-end">5</Text>
+        </div>
+      </div>
+      <div><Button>X</Button></div>
+    </Card>
+  )
+}
+
 function App() {
   return (
-    <main className={`${textVariant.default}`}>
-      <Text variant='muted' >Hello, World!</Text>
-      <Button>Click Me</Button>
+    <main className={`${textVariant.default} text-(--text) font-(--font-sans)`}>
+     <Calculator />
     </main>
   );
 }
