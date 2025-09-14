03fe3294baf572f107ba864f3c7fa96651c2221c
fevunge <fevunge@outlook.com>
2025-09-14 01:00:12 +0100

    learn: customize components

--- module-02/app.js
+++ module-02/app.js
@@ -3,15 +3,31 @@
 const textVariant = {
   default: "text-xl",
   muted: "text-xl text-(--text-secondary)",
-  heading: "text-2xl",
-  blast: "text-3xl",
+  heading: "text-2xl font-medium",
+  blast: "text-3xl font-medium",
+}
+
+function Text({as = 'span', children, className, variant, ...props}) {
+  const Component = as;
+  return <Component className={`${textVariant[variant]}  ${className}`} {...props}>{children}</Component>;
+}
+
+function Button({children, className, ...props}) {
+  return (
+    <Text as='button' variant='heading' 
+      className={`px-4 py-2 flex items-center justify-center rounded-xl cursor-pointer text(--text) bg-linear-(--gradient) hover:bg-linear-(--gradient-hover) ${className}`} 
+      {...props}
+    >
+      {children}
+    </Text>
+  );
 }
 
 function App() {
   return (
     <main className={`${textVariant.default}`}>
-      <h1>Hello, React 18!</h1>
-      <p>This is a simple React application.</p>
+      <Text variant='muted' >Hello, World!</Text>
+      <Button>Click Me</Button>
     </main>
   );
 }
